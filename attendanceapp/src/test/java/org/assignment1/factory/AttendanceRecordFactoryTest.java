ffb464a2007c295646a845df3e2a641094b1ba3f
Mckallan Bernardo <164789220+MckallanBernardo@users.noreply.github.com>
2025-03-28 23:42:54 +0200

    Update AttendanceRecordFactoryTest.java

--- attendanceapp/src/test/java/org/assignment1/factory/AttendanceRecordFactoryTest.java
+++ attendanceapp/src/test/java/org/assignment1/factory/AttendanceRecordFactoryTest.java
@@ -46,30 +46,4 @@ class AttendanceRecordFactoryTest {
     }
 
 
-    //    ArrayList <Student> student1 = new ArrayList<Student>();
-//    ArrayList <Teacher> teacher1 = new ArrayList<Teacher>();
-
-
-//    Classroom classroom1 = ClassroomFactory.createClassroom("987","English101",teacher1,student1);
-
-//    private static AttendanceRecordFactory attendanceRecordFactory1 = AttendanceRecordFactory.createAttendanceRecord("011",LocalDate.of(2021,3,10),
-//            "PRESENT",StudentFactory.createStudent("","","",LocalDate.of(2007,9,13)),
-//            TeacherFactory.createTeacher("2939","John","Stevens","English"), ;
-//
-//    @Test
-//    public void testCreateAttendanceRecord() {
-//        assertNotNull(attendanceRecordFactory1);
-//        System.out.println(attendanceRecordFactory1.toString());
-//    }
-//    @Test
-//    public void testCreateAttendanceRecordWithAllAttributes() {
-//        assertNotNull(attendanceRecordFactory1);
-//        System.out.println(attendanceRecordFactory1.toString());
-//    }
-//    @Test
-//    @Disabled
-//    public void testCreateAttendanceRecordThatFails(){
-//        assertNotNull(attendanceRecordFactory1);
-//        System.out.println(attendanceRecordFactory1.toString());
-//    }
 }
