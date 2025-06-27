4343ba87268d8aa7be28b8bb418ea7cfb75bd8ff
Aidan Coetzee <164910914+APC-123@users.noreply.github.com>
2025-06-27 14:13:39 +0200

    Teacher

---
+++ attendanceapp/src/main/java/za/ac/cput/domain/Teacher.java
@@ -0,0 +1,73 @@
+package za.ac.cput.domain;
+
+public class Teacher {
+
+    private String teacherID;
+    private String firstName;
+    private String lastName;
+    private String subject;
+
+
+    protected Teacher() {
+    }
+    private Teacher(Builder builder){
+        this.teacherID = builder.teacherID;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.subject = builder.subject;
+    }
+
+    public String getTeacherID() {
+        return teacherID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+    public static class Builder {
+        private String teacherID;
+        private String firstName;
+        private String lastName;
+        private String subject;
+
+        public Builder setTeacherID(String teacherID) {
+            this.teacherID = teacherID;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setSubject(String subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public Builder copy(Teacher teacher) {
+            this.teacherID = teacher.getTeacherID();
+            this.firstName = teacher.getFirstName();
+            this.lastName = teacher.getLastName();
+            this.subject = teacher.getSubject();
+            return this;
+        }
+
+        public Teacher build() {
+            return new Teacher(this);
+        }
+    }
+}
