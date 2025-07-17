92e56b899097b4161005d0ce12054e8c333841b9
Mckallan Bernardo <219018243@mycput.ac.za>
2025-07-17 11:01:19 +0200

    Updated classes with ClassroomFactoryTest class.

--- attendanceapp/src/main/java/za/ac/cput/domain/AttendanceReport.java
+++ attendanceapp/src/main/java/za/ac/cput/domain/AttendanceReport.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+
 @Entity
 public class AttendanceReport {
 
--- attendanceapp/src/test/java/za/ac/cput/factory/ClassroomFactoryTest.java
+++ attendanceapp/src/test/java/za/ac/cput/factory/ClassroomFactoryTest.java
@@ -1,11 +1,38 @@
 package za.ac.cput.factory;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Classroom;
+import za.ac.cput.domain.Student;
+import za.ac.cput.domain.Teacher;
+
+import java.util.Collections;
+import java.util.List;
+
 class ClassroomFactoryTest {
+
+    private ClassroomFactory factory;
+    private Teacher teacher;
+    private Student student1;
+    private List<Student> students;
+
+    @BeforeEach
+    void setUp() {
+        factory = new ClassroomFactory();
+
+        teacher = TeacherFactory.createTeacher("T001", "John", "Doe", "Mathematics");
+        student1 = StudentFactory.createStudent("S001", "Jane", "Smith");
+        students = Collections.singletonList(student1);
+    }
 
     @Test
     void createClassroom() {
+        Classroom classroom = factory.createClassroom("C001", "101", teacher, students);
+        assertNotNull(classroom);
+        assertEquals("C001", classroom.getClassroomId());
+        assertEquals("101", classroom.getClassName());
+        assertEquals(teacher, classroom.getTeacher());
+        assertIterableEquals(students, classroom.getStudent());
     }
 }
