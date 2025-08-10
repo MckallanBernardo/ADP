175994c6bcb8e8bf80657dcde40169b9a25a09af
Nur Amod <230543502@mycput.ac.za>
2025-08-10 23:23:14 +0200

    IStudentService, StudentServiceImpl, StudentController

---
+++ attendanceapp/src/main/java/za/ac/cput/service/IStudentService.java
@@ -0,0 +1,9 @@
+package za.ac.cput.service;
+
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Classroom;
+import za.ac.cput.domain.Student;
+
+public interface IStudentService extends IService<Student, String>{
+}
+
---
+++ attendanceapp/src/main/java/za/ac/cput/service/StudentServiceImpl.java
@@ -0,0 +1,47 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Student;
+import za.ac.cput.repository.IStudentRepository;
+
+import java.util.List;
+@Service
+public class StudentServiceImpl implements IStudentService {
+
+    private final IStudentRepository repository;
+
+    @Autowired
+    public StudentServiceImpl(IStudentRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Student create(Student newStudent) {
+        return this.repository.save(newStudent);
+    }
+
+    @Override
+    public Student read(String id) {
+        return this.repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Student update(Student student) {
+        return this.repository.save(student);
+    }
+
+    @Override
+    public void delete(String id) {
+        this.repository.deleteById(id);
+    }
+
+    @Override
+    public List<Student> getAll() {
+        return repository.findAll();
+    }
+    @Override
+    public Student findById(String id) {
+        return read(id);
+    }
+}
