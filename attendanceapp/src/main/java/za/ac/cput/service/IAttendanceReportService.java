de86330c3338b1cc0ddb8d8f5bc725009882e9de
Keagan Christians <230702732@mycput.ac.za>
2025-08-25 14:55:51 +0200

    commit AttendanceReport AttendanceReportController AttendanceReportControllerTest AttendanceReportService AttendanceReportServiceTest AttendanceReportFactoryTest IAttendanceReportService

---
+++ attendanceapp/src/main/java/za/ac/cput/service/IAttendanceReportService.java
@@ -0,0 +1,7 @@
+package za.ac.cput.service;
+
+import za.ac.cput.domain.AttendanceReport;
+
+public interface IAttendanceReportService extends IService<AttendanceReport, String> {
+    // any extra custom methods can go here if needed
+}
